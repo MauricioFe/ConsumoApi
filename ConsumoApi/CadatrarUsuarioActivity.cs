657d9300addbe0e8d2d9473f685c06c097f3051a
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-19 15:29:33 -0300

    Criando tela de cadastrar usuário

---
+++ ConsumoApi/CadatrarUsuarioActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ConsumoApi
+{
+    [Activity(Label = "CadatrarUsuarioActivity")]
+    public class CadatrarUsuarioActivity : Activity
+    {
+        EditText edtNome;
+        EditText edtEmail;
+        EditText edtTelefone;
+        EditText edtSenha;
+        Spinner spnfuncao;
+        Button btnSalvar;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_cadastrar_usuario);
+            edtNome = FindViewById<EditText>(Resource.Id.cadastrar_usuario_edt_nome);
+            edtEmail = FindViewById<EditText>(Resource.Id.cadastrar_usuario_edt_email);
+            edtTelefone = FindViewById<EditText>(Resource.Id.cadastrar_usuario_edt_telefone);
+            edtSenha = FindViewById<EditText>(Resource.Id.cadastrar_usuario_edt_senha);
+            spnfuncao = FindViewById<Spinner>(Resource.Id.cadastrar_usuario_spinner);
+            btnSalvar = FindViewById<Button>(Resource.Id.cadastrar_usuario_btn_salvar);
+
+        }
+    }
+}
